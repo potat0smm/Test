f4d2d3894159fe459229d9e00f7d6b013440247a
NN <stepan.morozov.18@inbox.ru>
2024-03-01 15:49:40 +0300

    Container

--- app/src/main/java/com/example/test/Main/MainFragment.kt
+++ app/src/main/java/com/example/test/Main/MainFragment.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.test.R
+import com.example.test.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding?=null
+    private val binding get () = _binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,39 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        replaceFragment(MenuFragment())
+
+        binding?.bottomNavigationView?.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.home -> replaceFragment(MenuFragment())
+                R.id.wallet -> replaceFragment(WalletFragment())
+                R.id.track -> replaceFragment(TrackFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+
+                else -> {}
+            }
+            true
+        }
+
+        return binding?.root
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        fragment?.let{
+            val fragmentManager = childFragmentManager
+            val fragmentTransition = fragmentManager.beginTransaction()
+            fragmentTransition.replace(R.id.frame_layout,it)
+            fragmentTransition.commit()
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
